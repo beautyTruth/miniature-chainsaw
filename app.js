48c58400d62af3231a4bb5f068395d89c90471a3
Phil <happy.melodist@gmail.com>
2021-12-08 04:10:40 -0600

    review markup and styling including an animation, formulating questions for the code to be learned

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const currentImg = document.querySelector(".current-img");
-const imgs = document.querySelectorAll(".all-imgs img");
+// const currentImg = document.querySelector(".current-img");
+// const imgs = document.querySelectorAll(".all-imgs img");
 
-imgs.forEach(function (img) {
-  img.addEventListener("click", function (e) {
-    currentImg.src = e.target.src;
+// imgs.forEach(function (img) {
+//   img.addEventListener("click", function (e) {
+//     currentImg.src = e.target.src;
 
-    currentImg.classList.add("animate-imgs");
-    setTimeout(function () {
-      currentImg.classList.remove("animate-imgs");
-    }, 400);
-  });
-});
+//     currentImg.classList.add("animate-imgs");
+//     setTimeout(function () {
+//       currentImg.classList.remove("animate-imgs");
+//     }, 400);
+//   });
+// });
+
+// practice kindness
+
+/*
+my code below
+*/
